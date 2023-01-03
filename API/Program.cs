eac14840806a013af3a1ff57edfe773a57b2fc7a
Bart Roels <roelsbart@gmail.com>
2023-01-03 12:02:28 +0100

    add HTTPS add CORS add user retrieval

--- API/Program.cs
+++ API/Program.cs
@@ -6,11 +6,15 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt => 
+builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaulConnection"));
 });
+builder.Services.AddCors();
+
 var app = builder.Build();
+
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 // Configure the HTTP request pipeline.
 app.MapControllers();
