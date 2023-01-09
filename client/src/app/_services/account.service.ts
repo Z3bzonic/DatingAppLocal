6d0b89c0d239ba158481584e829049df68fe5993
Bart Roels <roelsbart@gmail.com>
2023-01-09 09:50:21 +0100

    add register refresh db

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, map } from 'rxjs';
+import { BehaviorSubject, map, pipe } from 'rxjs';
 import { User } from '../_models/user';
 
 @Injectable({
@@ -22,6 +22,19 @@ export class AccountService {
           this.currentUsersource.next(user);
         }
       })
+    )
+  }
+
+  register(model: any){
+    return this.http.post<User>(this.baseUrl + 'account/register', model).pipe(
+      map(user => {
+        if(user){
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUsersource.next(user);
+        }
+        return user;
+      }
+        )
     )
   }
 
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,13 +8,15 @@ import { FormsModule } from '@angular/forms';
 // import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    HomeComponent
+    HomeComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
